dfc3875eb29818d478301fb04bd5bfbef12a36f2
EC2 Default User <ec2-user@ip-172-31-26-27.us-east-2.compute.internal>
2023-02-03 07:50:33 +0000

    code for hit counter added, dynamodb and lamda funtion with handler

--- cdk_workshop/cdk_workshop_stack.2.py
+++ cdk_workshop/cdk_workshop_stack.2.py
@@ -5,15 +5,12 @@ from aws_cdk import (
     aws_apigateway as apigw,
 )
 
-from .hitcounter import HitCounter
-
 
 class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Defines an AWS Lambda resource
         my_lambda = _lambda.Function(
             self, 'HelloHandler',
             runtime=_lambda.Runtime.PYTHON_3_7,
@@ -21,12 +18,8 @@ class CdkWorkshopStack(Stack):
             handler='hello.handler',
         )
 
-        hello_with_counter = HitCounter(
-            self, 'HelloHitCounter',
-            downstream=my_lambda,
-        )
-
         apigw.LambdaRestApi(
             self, 'Endpoint',
-            handler=hello_with_counter._handler,
+            handler=my_lambda,
         )
+        
