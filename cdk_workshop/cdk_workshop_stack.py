2783dcd386e9bee6e086dbb203cee1edc2e9c6e9
EC2 Default User <ec2-user@ip-172-31-26-27.us-east-2.compute.internal>
2023-02-03 04:22:56 +0000

    first commit

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,26 +1,25 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
 
 
 class CdkWorkshopStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
         )
 
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=my_lambda,
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
