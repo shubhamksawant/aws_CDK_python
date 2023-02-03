62b7930ae221cba8fba2e067b2ee3dd5eaf72837
EC2 Default User <ec2-user@ip-172-31-26-27.us-east-2.compute.internal>
2023-02-03 09:00:16 +0000

    updated  cdk_workshop/cdk_workshop_stack.py,  cdk_workshop/hitcounter.py,    requirements.txt and added copy file  cdk_workshop/cdk_workshop_stack.3.py

--- cdk_workshop/cdk_workshop_stack.3.py
+++ cdk_workshop/cdk_workshop_stack.3.py
@@ -5,7 +5,6 @@ from aws_cdk import (
     aws_apigateway as apigw,
 )
 
-from cdk_dynamo_table_view import TableViewer
 from .hitcounter import HitCounter
 
 
@@ -15,7 +14,7 @@ class CdkWorkshopStack(Stack):
         super().__init__(scope, id, **kwargs)
 
         # Defines an AWS Lambda resource
-        hello = _lambda.Function(
+        my_lambda = _lambda.Function(
             self, 'HelloHandler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
@@ -24,16 +23,10 @@ class CdkWorkshopStack(Stack):
 
         hello_with_counter = HitCounter(
             self, 'HelloHitCounter',
-            downstream=hello,
+            downstream=my_lambda,
         )
 
         apigw.LambdaRestApi(
             self, 'Endpoint',
             handler=hello_with_counter._handler,
-        )
-
-        TableViewer(
-            self, 'ViewHitCounter',
-            title='Hello Hits',
-            table=hello_with_counter.table,
         )
